fcf76fe9c4588a7e5cb7003c632d8518d70179fa
MasoudPirhadi <masoud.mp78@gmail.com>
2025-07-26 08:38:13 +0330

    fix: env file

--- ai_application/settings.py
+++ ai_application/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_render_partial',
+    'dotenv',
     'openai',
     'ai_module',
     'account_module',
